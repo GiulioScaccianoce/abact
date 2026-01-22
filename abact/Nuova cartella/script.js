09674f6e98568beb29105210b67c3cb5509db009
GiulioScaccianoce <giulioscaccianoce@gmail.com>
2026-01-22 20:22:31 +0100

    Add files via upload

---
+++ abact/Nuova cartella/script.js
@@ -0,0 +1,12 @@
+<script>
+function removeElement() {
+  document.getElementById("carattere1").style.display = "none";
+}
+
+function resetElement() {
+  document.getElementById("carqttere1").style.display = "block";
+}
+</script>
+
+//sistemare comparsa e scomparsa slide al click 
+// stato attuale: non funziona 
